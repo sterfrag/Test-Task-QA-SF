a539959aa58b965f094ee65ea372481d8bd50019
Fragkos Stergios <52353726+sterfrag@users.noreply.github.com>
2023-05-17 16:49:39 +0300

    Update run_server.bat . Deleted unecessary command

--- run_server.bat
+++ run_server.bat
@@ -1,6 +1,2 @@
 @echo off
 "C:\Users\Stelios\Anaconda3\python.exe" "C:\Users\Stelios\Desktop\CODE_TEST\02_Python\api\api_server.py"
-@echo off
-cd ".\Desktop\CODE_TEST\02_Python\api\" "python.exe  pytest -v --html=test_suite_report.htl"
-
-pause
